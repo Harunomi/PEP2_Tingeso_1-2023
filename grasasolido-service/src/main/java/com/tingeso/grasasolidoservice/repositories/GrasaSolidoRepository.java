e9425e496e03b39698e3a22139da3408f32cd380
unknown <cristhofer.parada@usach.cl>
2023-06-22 00:41:39 -0400

    clean up

--- grasasolido-service/src/main/java/com/tingeso/grasasolidoservice/repositories/GrasaSolidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tingeso.grasasolidoservice.repositories;
-
-import com.tingeso.grasasolidoservice.entities.GrasaSolidoEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GrasaSolidoRepository extends CrudRepository<GrasaSolidoEntity,Long> {
-}
--- planilla-service/src/main/java/com/tingeso/planillaservice/repositories/PlanillaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tingeso.planillaservice.repositories;
-
-import com.tingeso.planillaservice.entities.PlanillaEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanillaRepository extends CrudRepository<PlanillaEntity,Long> {
-}
